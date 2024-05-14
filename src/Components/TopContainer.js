2f2b3fe97d6ea4f11e84adc93645c6a5ffcc75c5
sivaprasath2004 <prasathsiva2004@gmail.com>
2024-05-14 19:07:28 +0530

    search results fixes

--- src/Components/TopContainer.js
+++ src/Components/TopContainer.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import { BiSearchAlt } from "react-icons/bi";
 import { FaBell, FaChevronDown } from "react-icons/fa";
 import women from "../img/women.jpg";
-
+import search from "./searchResults";
 function TopContainer() {
+  const [input,setInput]=useState("")
+  const [searchResult,setSearchResult]=useState(null)
   useEffect(() => {
     const mouseTarget = document.getElementById("menuChevron");
     const menuContainer = document.getElementById("menuContainer");
@@ -17,14 +19,35 @@ function TopContainer() {
       menuContainer.style.transform = "translateX(300px)";
     });
   }, []);
-
+useEffect(()=>{
+ if(input.length>0){
+  const results=search.filter(item=>item.title.toLowerCase().includes(input.toLocaleLowerCase()));
+  setSearchResult(results)
+ }
+ else{
+  setSearchResult(null)
+ }
+},[input])
   return (
     <div className="topContainer">
       <div className="inputBox">
-        <input type="text" placeholder="Search projects, users" />
+        <input type="text" placeholder="Search projects, users" onChange={(e)=>setInput(e.target.value)} />
         <i>
           <BiSearchAlt />
         </i>
+        {searchResult?
+      <div id="search_results">
+        {
+          searchResult.map((item,index)=>(
+            <div key={`parent_container${index}`}>
+              <p id='search_result_title' key={`title${index}`}>{item.title}</p>
+              <span key={`category${index}`}>{item.category}</span>
+            </div>
+          ))
+        }
+      </div>
+      :<></>
+        }
       </div>
 
       <div className="profileContainer">
