ab61bbab37402596948b73bd4f397761b0e57d8c
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-05-22 00:56:08 +0530

    added about section of website

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Container from "./Components/Container";
 import Footer from "./Components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Settings from "./Components/Settings/SettingsPage";
+import About_us from "./Components/footer_section/about_us/About_us";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Container />} />
           <Route path="/settings" element={<Settings />} />
+          <Route path="/about" element={<About_us/>}/>
         </Routes>
         <Footer />
       </div>
