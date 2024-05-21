a0308e9501862b8c8a17c19d321701f88eb49146
Tejaswa Jhode <jhodetejswa2004@gmail.com>
2024-05-21 12:29:42 +0530

    final

--- src/App.js
+++ src/App.js
@@ -2,26 +2,18 @@
 import "./App.css";
 import Menu from "./Components/Menu";
 import Container from "./Components/Container";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Wallet from "./Components/Wallet";
 import Footer from "./Components/Footer";
 
 function App() {
   return (
-   <Router>
+  
         <div className="App">
        <div className="App-content">
-         <Menu />
-         
-         <Routes>
-           <Route path="/wallet" element={<Wallet />} />
-           </Routes>
-
          <Container />
        </div>
        <Footer />
      </div>
-    </Router>
+  
   );
 }
 
--- src/Components/Container.js
+++ src/Components/Container.js
@@ -2,17 +2,25 @@ import React from "react";
 import "./Container.css";
 import TopContainer from "./TopContainer";
 import MainContainer from "./MainContainer";
-
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Wallet from "./Wallet";
+import Menu from "./Menu";
 function Container() {
   return (
+
     <div className="container">
-     
-      <div className="content">
+       <Router>
+       <Menu />
+         <Routes>
+           <Route path="/wallet" element={<Wallet />} />
+           </Routes>
+       <div className="content">
         <TopContainer />
         <MainContainer />
       </div>
+      </Router>
     </div>
+    
   );
 }
 
