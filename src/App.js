5baa994c59c27c67fe910e664a68b4cdb4d3cba1
Aastha Kourav <aasthakourav2002@gmail.com>
2024-05-27 17:23:56 +0530

    adding services

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Footer from "./Components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Settings from "./Components/Settings/SettingsPage";
 import About_us from "./Components/footer_section/about_us/About_us";
-import BlogPage from "./Components/footer_section/BlogPage/BlogPage"
+import BlogPage from "./Components/footer_section/BlogPage/BlogPage" ;
+import Services from "./Components/footer_section/services/Services";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/settings" element={<Settings />} />
           <Route path="/about" element={<About_us />} />
           <Route path="/blogs" element={<BlogPage />} />
+          <Route path="/services" element={<Services />} />
         </Routes>
         <Footer />
       </div>
