c73f05fe7db3ce48a1a55247fa6f5df127479a80
dharshib <Dharshib.8@gmail.com>
2024-05-30 15:10:49 +0530

    update new project form input errors

--- src/Components/menu_section/new_project/NewProject.js
+++ src/Components/menu_section/new_project/NewProject.js
@@ -9,31 +9,33 @@ const Skills = ['HTML', 'CSS', 'Javascript', 'Python', 'Java', 'React js', 'Angu
 
 function NewProject() {
 
-    const [data, setData] = useState({name: "", github: "", description: "", image: null, skill: ""});
-    const [suggestions, setSuggestions] = useState([])
+    const [data, setData] = useState({ name: "", github: "", description: "", image: null, skill: "" });
+    const [skillSuggestions, setSuggestions] = useState([])
     const [selectedSkills, setSelectedSkills] = useState([])
-    const [error, setError] = useState("")
+    const [error, setError] = useState([])
 
-    const onInputChange = (name, value) => {
-        setData(prev => ({...prev, [name]: value}));
-        console.log(data)
+    const onInputChange = (e) => {
+        const { name, value } = e.target
+        setData(prev => ({ ...prev, [name]: value }));
+        setError([])
     }
 
-    const onSkillChange = (val) => {
-        if (val === "") {
+    const onSkillChange = (e) => {
+        const { value } = e.target
+        if (value === "") {
             setSuggestions([])
         }
         else {
-            const arr = Skills.filter(skill => skill.toLowerCase().startsWith(val.toLowerCase())).slice(0, 5)
+            const arr = Skills.filter(skill => skill.toLowerCase().startsWith(value.toLowerCase())).slice(0, 5)
             setSuggestions(arr);
         }
-        onInputChange("skill", val)
+        setData(prev => ({ ...prev, "skill": value }));
     }
 
     const addSkill = (val) => {
-        if (selectedSkills.length ===5) return;
-        const existing = selectedSkills.filter(skill => val===skill)
-        if (existing.length===0) {
+        if (selectedSkills.length === 5) return;
+        const existing = selectedSkills.filter(skill => val === skill)
+        if (existing.length === 0) {
             setSelectedSkills(prev => [...prev, val])
         }
         setSuggestions([])
@@ -46,14 +48,14 @@ function NewProject() {
     }
 
     const onImageChange = (e) => {
-        setData(prev => ({...prev, "image": e.target.files[0]}));
+        setData(prev => ({ ...prev, "image": e.target.files[0] }));
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
         const err = validateData(data, selectedSkills);
         setError(err)
-        if (err!=="") {
+        if (err.length !== 0) {
             window.scroll(0, 0)
         }
     }
@@ -71,58 +73,61 @@ function NewProject() {
                 <form onSubmit={handleSubmit}>
 
                     {
-                        (error !== "") && (
-                            <small 
+                        (error.length !== 0) && (
+                            <div
                                 className='error'
                             >
-                                <FaTimes />
-                                {error}
-                            </small>
+                                {
+                                    error.map(err => {
+                                        return <small>ERROR: &nbsp;  {err} <br /></small>
+                                    })
+                                }
+                            </div>
                         )
                     }
                     <div className='input-box'>
                         <label>
                             Project Name: <span>*</span>
-                            <input 
-                                type='text' 
-                                placeholder='Eg. Weather app' 
+                            <input
+                                type='text'
+                                placeholder='Eg. Weather app'
                                 name="name"
                                 value={data.name}
-                                onChange={(e) => onInputChange(e.target.name, e.target.value)}
+                                onChange={onInputChange}
                             />
                         </label>
                     </div>
                     <div className='input-box'>
                         <label>
                             GitHub Repository Link: <span>*</span>
-                            <input 
-                                type='url' 
+                            <input
+                                type='url'
                                 name="github"
                                 value={data.github}
-                                placeholder='Eg. https://github.com/your-username/repository-name' 
-                                onChange={(e) => onInputChange(e.target.name, e.target.value)}
+                                placeholder='Eg. https://github.com/your-username/repository-name'
+                                onChange={onInputChange}
                             />
-                            
+
                         </label>
                     </div>
                     <div className='input-box'>
                         <label>
                             Description: <span>*</span>
-                            <textarea 
-                                placeholder='Share about what makes your project special?' 
-                                rows={15} 
+                            <textarea
+                                placeholder='Share about what makes your project special?'
+                                rows={15}
                                 name="description"
                                 value={data.description}
-                                onChange={(e) => onInputChange(e.target.name, e.target.value)}
+                                onChange={onInputChange}
                             />
                         </label>
                     </div>
                     <div className='input-box'>
                         <label>
                             Image: <span>*</span>
-                            <input 
-                                type='file' 
-                                onChange={(e) => onImageChange(e)}
+                            <input
+                                type='file'
+                                onChange={onImageChange}
                                 accept='image/*'
                             />
                         </label>
@@ -130,21 +135,21 @@ function NewProject() {
                     <div className='input-box'>
                         <label>
                             Skills (Add skills upto to 5, so users can know what your project about.): <span>*</span>
-                            <input 
-                                type='text' 
+                            <input
+                                type='text'
                                 placeholder='Eg. JAVA, React js...'
                                 name='skill'
                                 value={data.skill}
-                                onChange={e => onSkillChange(e.target.value)}
-                                disabled={selectedSkills.length===5}
+                                onChange={onSkillChange}
+                                disabled={selectedSkills.length === 5}
                             />
                             {
-                                (suggestions.length > 0) && (
+                                (skillSuggestions.length > 0) && (
                                     <ul className='suggesion-list'>
                                         {
-                                            suggestions.slice(0, 5).map(sug => {
+                                            skillSuggestions.slice(0, 5).map(sug => {
                                                 return (
-                                                    <li 
+                                                    <li
                                                         key={sug}
                                                         onClick={() => addSkill(sug)}
                                                     >
@@ -160,12 +165,12 @@ function NewProject() {
                                 {
                                     selectedSkills.map(skill => {
                                         return (
-                                            <li 
+                                            <li
                                                 key={skill}
                                             >
                                                 {skill}
-                                                <FaTimes 
-                                                    size={13} 
+                                                <FaTimes
+                                                    size={13}
                                                     onClick={() => removeSkill(skill)}
                                                 />
                                             </li>
@@ -189,15 +194,15 @@ export default NewProject;
 
 
 function validateData(data, skills) {
-    let error = "";
+    let errors = [];
     const ImageMaxSize = 10 * 1024 * 1024;
 
-    if (data.name.trim() === "" ) error+="Project name is required! "
-    if (data.github.trim() === "")  error+="Github repo link is required! "
-    if (data.description.trim() === "")  error+="Description is required! "
-    if (skills.length === 0)  error+="Select atleast one skill! "
-    if (!data.image)  error+="Project image is required! "
-    else if (data.image.size > ImageMaxSize)  error+="Select an image, not exceeding 10MB! "
+    if (data.name.trim() === "") errors.push("Project name is required.")
+    if (data.github.trim() === "") errors.push("Github repo link is required.")
+    if (data.description.trim() === "") errors.push("Description is required.")
+    if (skills.length === 0) errors.push("Select atleast one skill.")
+    if (!data.image) errors.push("Project image is required.")
+    else if (data.image.size > ImageMaxSize) errors.push("Select an image, not exceeding 10MB. ")
 
-    return error;
+    return errors;
 }
