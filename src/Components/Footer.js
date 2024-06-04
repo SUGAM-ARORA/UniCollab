401b85d631f0f240564257fb107aa233cb214333
Jayanth <jayanththalla33@gmail.com>
2024-06-04 22:51:01 +0530

    footer add discord logo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Footer.css';
 import logo from '../img/apple-touch-icon.png';
+import discordLogo from '../img/discord.png';
 
 const Footer = () => {
   return (
     <div className='footer'>
       <div className='footer-container footer-container-md'>
         <div className='footer-1'>
-          <img src={logo} alt="logo"/>
+          <img src={logo} alt="logo" />
           <div className='footer-title'>Unicollab</div>
         </div>
         <div className='footer-half footer-sections'>
@@ -40,19 +41,21 @@ const Footer = () => {
         </div>
       </div>
       <div className='footer-icons footer-icons-md'>
-      <a href="#" target="_blank" rel='noreferrer'>
-          <i class="devicon-linkedin-plain"></i>
+        <a href="https://linkedin.com" target="_blank" rel='noreferrer'>
+          <i className="devicon-linkedin-plain linkedin"></i>
         </a>
         <a href="https://github.com/SUGAM-ARORA/UniCollab" target="_blank" rel='noreferrer'>
-          <i class="devicon-github-original"></i>
+          <i className="devicon-github-original github"></i>
         </a>
-        <a href="#" target="_blank" rel='noreferrer'>
-          <i class="devicon-facebook-plain"></i>
+        <a href="https://facebook.com" target="_blank" rel='noreferrer'>
+          <i className="devicon-facebook-plain facebook"></i>
         </a>
-        <a href="#" target="_blank" rel='noreferrer'>
-          <i class="devicon-twitter-original"></i>
+        <a href="https://twitter.com" target="_blank" rel='noreferrer'>
+          <i className="devicon-twitter-original twitter"></i>
         </a>
-
+        <a href="https://discord.com" target="_blank" rel='noreferrer'>
+          <img src={discordLogo} alt="Discord" className="discord" />
+        </a>
       </div>
       <div className='footer-bottom'>
         <div className='footer-bottom-link'>
