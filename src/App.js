9b6c46af780afda2adda92425349934edbdc56d3
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-07-06 22:58:44 +0530

    Connect with The Chatbot

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import "./App.css";
-import Container from "./Components/Container";
-import Footer from "./Components/Footer";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Settings from "./Components/Settings/SettingsPage";
-import About_us from "./Components/footer_section/about_us/About_us";
-import BlogPage from "./Components/footer_section/BlogPage/BlogPage";
-import FAQPage from "./Components/footer_section/FAQPage/FAQPage";
-import Services from "./Components/footer_section/services/Services";
-import ContactUs from "./Components/footer_section/ContactUs/contact_us";
-import Readmore from "./Components/Readmore";
-import Login from "./Components/Login";
-import Pricing from "./Components/footer_section/Pricing/pricing";
-import MyProjects from "./Components/menu_section/my_projects/MyProjects";
-import NewProject from "./Components/menu_section/new_project/NewProject";
-import Error from "./Components/404_page/Error";
-import Profiles from "./Components/Profiles";
-import Profile from "./Components/Profile/profile";
-import Licensing from "./Components/footer_section/Legal/Licensing";
-import TermsConditions from "./Components/footer_section/Legal/TermsandConditions";
-import PrivacyPolicy from "./Components/footer_section/Legal/PrivacyPolicy";
-import Careers from "./Components/Careers/CareersPage";
-import EmailVerification from "./Components/auth/resetPassword/EmailVerification";
-import OTPVerification from "./Components/auth/resetPassword/OTPVerification";
-import ResetPassword from "./Components/auth/resetPassword/ResetPassword";
-import Freelancer from "./Components/FreeLancer/Freelancer";
-import router from "./configs/router";
-// Define the routes
-
+import React from 'react';
+import './App.css';
+import Container from './Components/Container';
+import Footer from './Components/Footer';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Settings from './Components/Settings/SettingsPage';
+import About_us from './Components/footer_section/about_us/About_us';
+import BlogPage from './Components/footer_section/BlogPage/BlogPage';
+import FAQPage from './Components/footer_section/FAQPage/FAQPage';
+import Services from './Components/footer_section/services/Services';
+import ContactUs from './Components/footer_section/ContactUs/contact_us';
+import Readmore from './Components/Readmore';
+import Login from './Components/Login';
+import Pricing from './Components/footer_section/Pricing/pricing';
+import MyProjects from './Components/menu_section/my_projects/MyProjects';
+import NewProject from './Components/menu_section/new_project/NewProject';
+import Error from './Components/404_page/Error';
+import Profiles from './Components/Profiles';
+import Profile from './Components/Profile/profile';
+import Licensing from './Components/footer_section/Legal/Licensing';
+import TermsConditions from './Components/footer_section/Legal/TermsandConditions';
+import PrivacyPolicy from './Components/footer_section/Legal/PrivacyPolicy';
+import Careers from './Components/Careers/CareersPage';
+import EmailVerification from './Components/auth/resetPassword/EmailVerification';
+import OTPVerification from './Components/auth/resetPassword/OTPVerification';
+import ResetPassword from './Components/auth/resetPassword/ResetPassword';
+import Freelancer from './Components/FreeLancer/Freelancer';
+import router from './configs/router';
+import ChatbotIcon from './Components/ChatbotIcon';
 
 function App() {
   return (
@@ -35,8 +35,8 @@ function App() {
       <div>
         <RouterProvider router={router} />
       </div>
-
       <Footer />
+      <ChatbotIcon />
     </div>
   );
 }
