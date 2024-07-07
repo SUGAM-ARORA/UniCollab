c9e8c8dead63f8f9f81ac53794b6d0f6cfd956af
chaanakyaaM <chaanakyaam@gmail.com>
2024-07-07 18:53:10 +0530

    SEO tags are added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,50 @@
-import React from 'react';
-import './App.css';
-import Container from './Components/Container';
-import Footer from './Components/Footer';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Settings from './Components/Settings/SettingsPage';
-import About_us from './Components/footer_section/about_us/About_us';
-import BlogPage from './Components/footer_section/BlogPage/BlogPage';
-import FAQPage from './Components/footer_section/FAQPage/FAQPage';
-import Services from './Components/footer_section/services/Services';
-import ContactUs from './Components/footer_section/ContactUs/contact_us';
-import Readmore from './Components/Readmore';
-import Login from './Components/Login';
-import Pricing from './Components/footer_section/Pricing/pricing';
-import MyProjects from './Components/menu_section/my_projects/MyProjects';
-import NewProject from './Components/menu_section/new_project/NewProject';
-import Error from './Components/404_page/Error';
-import Profiles from './Components/Profiles';
-import Profile from './Components/Profile/profile';
-import Licensing from './Components/footer_section/Legal/Licensing';
-import TermsConditions from './Components/footer_section/Legal/TermsandConditions';
-import PrivacyPolicy from './Components/footer_section/Legal/PrivacyPolicy';
-import Careers from './Components/Careers/CareersPage';
-import EmailVerification from './Components/auth/resetPassword/EmailVerification';
-import OTPVerification from './Components/auth/resetPassword/OTPVerification';
-import ResetPassword from './Components/auth/resetPassword/ResetPassword';
-import Freelancer from './Components/FreeLancer/Freelancer';
-import router from './configs/router';
+import "./App.css";
+import Container from "./Components/Container";
+import Footer from "./Components/Footer";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Settings from "./Components/Settings/SettingsPage";
+import About_us from "./Components/footer_section/about_us/About_us";
+import BlogPage from "./Components/footer_section/BlogPage/BlogPage";
+import FAQPage from "./Components/footer_section/FAQPage/FAQPage";
+import Services from "./Components/footer_section/services/Services";
+import ContactUs from "./Components/footer_section/ContactUs/contact_us";
+import Readmore from "./Components/Readmore";
+import Login from "./Components/Login";
+import Pricing from "./Components/footer_section/Pricing/pricing";
+import MyProjects from "./Components/menu_section/my_projects/MyProjects";
+import NewProject from "./Components/menu_section/new_project/NewProject";
+import Error from "./Components/404_page/Error";
+import Profiles from "./Components/Profiles";
+import Profile from "./Components/Profile/profile";
+import Licensing from "./Components/footer_section/Legal/Licensing";
+import TermsConditions from "./Components/footer_section/Legal/TermsandConditions";
+import PrivacyPolicy from "./Components/footer_section/Legal/PrivacyPolicy";
+import Careers from "./Components/Careers/CareersPage";
+import EmailVerification from "./Components/auth/resetPassword/EmailVerification";
+import OTPVerification from "./Components/auth/resetPassword/OTPVerification";
+import ResetPassword from "./Components/auth/resetPassword/ResetPassword";
+import Helmet from "react-helmet";
+import Freelancer from "./Components/FreeLancer/Freelancer";
+import router from "./configs/router";
 import ChatbotIcon from './Components/ChatbotIcon';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Helmet>
+        <meta name="description" content="Welcome to UniCollab, the ultimate platform for student collaboration. Connect with peers from various universities and colleges to innovate and elevate your projects. UniCollab: where ideas flow, knowledge is shared, and learning transcends classrooms."/>
+        <meta name="language" content="English"/>
+        <meta name="keywords" content="Collab, Education, Students, Student Collaboration, University Collaboration, College Projects, Innovative Learning, Peer Networking, Academic Collaboration, Project Sharing, Knowledge Sharing, Student Innovation, Collaborative Learning, University Networking, Inter-University Projects, Academic Networking, Collaborative Projects, Student Platform" />
+        <meta property="og:type" content="website" />
+        <meta name="author" content="Sugma Arora" />
+        <meta property="og:title" content="Unicollab" />
+        <meta property="og:description" content="Welcome to UniCollab, the ultimate platform for student collaboration. Connect with peers from various universities and colleges to innovate and elevate your projects. UniCollab: where ideas flow, knowledge is shared, and learning transcends classrooms." />
+        <meta property="og:image" content="https://github.com/SUGAM-ARORA/UniCollab/blob/main/image.png" />
+        <meta property="og:url" content="https://uni-collab.vercel.app/" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Helmet>
+      <div className="App">
       <div className="App-content"></div>
       <div>
         <RouterProvider router={router} />
@@ -38,6 +52,7 @@ function App() {
       <Footer />
       <ChatbotIcon />
     </div>
+    </>
   );
 }
 
