34c2507fc24fe67a813ac290d7b5ca93b96c5785
Sugam Arora <sugam.arora23@gmail.com>
2024-07-14 22:43:36 +0530

    Firebase Updated

--- Firebase/firebase.js
+++ Firebase/firebase.js
@@ -2,7 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,6 +24,8 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-export { db, auth };
+export { auth, provider, signInWithPopup };
+export { db, analytics, app };
 export default app;
