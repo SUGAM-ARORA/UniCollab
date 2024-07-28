4791b4de254a6cd8d62d31b89ea88714f20e09a8
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-07-28 13:21:39 +0530

    Implementation of Speed Page

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Menu.css";
 import logo from "../img/logo.png";
 import { Link } from "react-router-dom";
-
+import Stopwatch from "./Stopwatch"; // Import the Stopwatch component
 
 import {
   FaDelicious,
@@ -12,14 +12,10 @@ import {
   FaRegClock,
   FaCog,
   FaSignOutAlt,
-  FaCreativeCommons,
-  FaBlog,
-  FaSave,
   FaList,
 } from "react-icons/fa";
 
 function Menu() {
-
   let toggle = false;
 
   useEffect(() => {
@@ -36,11 +32,11 @@ function Menu() {
   }, []);
 
   const showDropDown = () => {
-    if(!toggle){
+    if (!toggle) {
       document.getElementById("mainMenu").style.display = "flex";
       document.getElementById("lastMenu").style.display = "flex";
       toggle = true;
-    } else{
+    } else {
       document.getElementById("mainMenu").style.display = "none";
       document.getElementById("lastMenu").style.display = "none";
       toggle = false;
@@ -48,51 +44,64 @@ function Menu() {
   };
 
   const [isMobile, setIsMobile] = useState(window.innerWidth < 524);
+  const [showStopwatch, setShowStopwatch] = useState(false); // State to handle stopwatch visibility
 
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth < 524);
     };
 
-    window.addEventListener('resize', handleResize);
-    // Clean up the event listener on component unmount
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
+  const toggleStopwatch = () => {
+    setShowStopwatch(!showStopwatch);
+  };
+
   return (
-
-    <menu className="fromLeft">
-    <Link href='/'>
-      <img src={logo} alt='icon' className="logo" id='logo' onClick={isMobile ? showDropDown : null}
-        style={{ cursor: isMobile ? 'pointer' : 'default' }}/>
-    </Link>
-
-      <ul className="fromTop" id="mainMenu">
-        <Icon icon={<FaList />} tooltip="My projects" href="/projects"/>
-        <Icon icon={<FaDelicious />} tooltip="Delicious" href="/" />
-        <Icon icon={<FaShoppingCart />} tooltip="Cart" href="/cart" />
-        <Icon icon={<FaWallet />} tooltip="Wallet" href="/" />
-        <Icon icon={<FaChartLine />} tooltip="Trending" href="/" />
-        <Icon icon={<FaRegClock />} tooltip="Speed" href="/" />
-      </ul>
-
-      <ul className='lastMenu' id='lastMenu'>
-        <Link to='/settings'>
-          <Icon icon={<FaCog />} tooltip='Settings' />
+    <>
+      <menu className="fromLeft">
+        <Link to="/">
+          <img
+            src={logo}
+            alt="icon"
+            className="logo"
+            id="logo"
+            onClick={isMobile ? showDropDown : null}
+            style={{ cursor: isMobile ? "pointer" : "default" }}
+          />
         </Link>
-        <Icon icon={<FaSignOutAlt />} tooltip='Sign Out' href='/' />
-      </ul>
-    </menu>
+
+        <ul className="fromTop" id="mainMenu">
+          <Icon icon={<FaList size={30} />} tooltip="My projects" href="/projects" />
+          <Icon icon={<FaDelicious size={30} />} tooltip="Delicious" href="/" />
+          <Icon icon={<FaShoppingCart size={30} />} tooltip="Cart" href="/cart" />
+          <Icon icon={<FaWallet size={30} />} tooltip="Wallet" href="/" />
+          <Icon icon={<FaChartLine size={30} />} tooltip="Trending" href="/" />
+          <Icon icon={<FaRegClock size={30} />} tooltip="Speed" href="#" onClick={toggleStopwatch} /> {/* Add onClick handler */}
+        </ul>
+
+        <ul className="lastMenu" id="lastMenu">
+          <Link to="/settings">
+            <Icon icon={<FaCog size={30} />} tooltip="Settings" />
+          </Link>
+          <Icon icon={<FaSignOutAlt size={30} />} tooltip="Sign Out" href="/" />
+        </ul>
+      </menu>
+
+      {showStopwatch && <Stopwatch onClose={toggleStopwatch} />} {/* Render the Stopwatch conditionally */}
+    </>
   );
 }
 
-const Icon = ({ icon, tooltip, href }) => (
+const Icon = ({ icon, tooltip, href, onClick }) => (
   <li>
-    <a href={href}>
+    <a href={href} onClick={(e) => { e.target.blur(); if(onClick) onClick(); }} style={{ display: "flex", alignItems: "center" }}>
       {icon}
-      <span className='tooltip'>{tooltip}</span>
+      <span className="tooltip">{tooltip}</span>
     </a>
   </li>
 );
