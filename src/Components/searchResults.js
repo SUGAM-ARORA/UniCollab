b387c49338f6807a53cfcfd9ba260992c45bf9b8
Garima <sushri4tech@gmail.com>
2024-07-29 21:33:25 +0530

    Enhance Search Functionality and Update Project Listing
    - Implemented search bar functionality with dynamic filtering.
    - Added links to search results for better navigation.
    - Fixed issues with project listing to ensure accurate display and filtering.

--- src/Components/searchResults.js
+++ src/Components/searchResults.js
@@ -1,15 +1,16 @@
 const search = [
-    { title: "StockIT", category: "Project" },
-    { title: "TakeNote", category: "Project" },
-    { title: "TaRct", category: "Project" },
-    { title: "To Do", category: "Project" },
-    { title: "ArchiTect", category: "Project" },
-    { title: "WeatherLy", category: "Project" },
-    {title: "Jane Cooper", category:"@JaneCooper" },
-    {title: "Ariene McCoy", category:  "@ArieneMcCoy" },
-    {title: "Theresa Webb", category: "@TheresaWebb" },
-    {title: "Ralph Edwards", category: "@RalphEdwards" },
-    {title: "Raplhie", category: "@Raplhie" }
+    {id: 1, title: "StockIT", category: "Project" },
+    {id: 2, title: "TakeNote", category: "Project" },
+    {id: 3, title: "TaRct", category: "Project" },
+    {id: 4, title: "To Do", category: "Project" },
+    {id: 5, title: "ArchiTect", category: "Project" },
+    {id: 6, title: "WeatherLy", category: "Project" },
+    {id: 7, title: "Typing Test", category:"@JaneCooper" },
+    {id: 8, title: "Artisan", category:  "@ArieneMcCoy" },
+    {id: 9, title: "BBlocks", category: "@TheresaWebb" },
+    {id: 10,title: "ZzShoes", category: "@RalphEdwards" },
+    {id: 11,title: "SearchEngine", category: "@Raplhie" },
+    {id: 12, title: "Alimage" , category: "Project"}
       
 ];
 export default search
