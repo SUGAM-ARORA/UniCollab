7db6c4b0415f04e02c60a19be575a47c187e3988
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-08-03 18:49:05 +0530

    UI Improvement

--- src/Components/wallet/Wallet.js
+++ src/Components/wallet/Wallet.js
@@ -1,10 +1,53 @@
-import React from 'react'
-import './Wallet.css'
+import React from 'react';
+import { Link } from "react-router-dom";
+import homeIcon from '../../img/homeicon.png';
+import './Wallet.css';
 
 const Wallet = () => {
+    const pointsHistory = [
+        {
+            description: "Purchase of StockIT subscription",
+            date: "12-07-2024",
+            points: 100
+        },
+        {
+            description: "Refer a friend to UniCollab",
+            date: "05-07-2024",
+            points: 50
+        },
+        {
+            description: "Participation in UniCollab survey",
+            date: "01-07-2024",
+            points: 30
+        },
+        {
+            description: "Weekly login bonus",
+            date: "28-06-2024",
+            points: 20
+        },
+        {
+            description: "Achievement unlocked: Complete 10 tasks",
+            date: "20-06-2024",
+            points: 150
+        },
+        {
+            description: "User feedback reward",
+            date: "15-06-2024",
+            points: 40
+        },
+        {
+            description: "Referral bonus for new user",
+            date: "10-06-2024",
+            points: 70
+        }
+    ];
+
     return (
         <div>
             <div className="heading1Wallet">Balance and Rewards</div>
+            <Link to="/">
+                <img src={homeIcon} alt="Home" className="home-icon" />
+            </Link>
             <div className='pointsandrupees'>
                 <div className='iconsWallet'>
                     <div className='quantity'>5000</div>
@@ -19,36 +62,25 @@ const Wallet = () => {
                 </div>
             </div>
             <div className='infoWallet'>
-                <div className='topic_head feedbackTopic'>UniCollab <span className='Pro'>Pro</span></div>
-                <div className='descWallet1'>
-                    <div className='quantity'>Now, UniCollab Pro members will earn 50 ppoints</div>
-                    <div className='value'>On every ₹100 spent on UniCollab</div>
-                </div>
-            </div>
+    <div className='topic_head feedbackTopic'>UniCollab <span className='Pro'>Pro</span></div>
+    <div className='descWallet1'>
+        <div className='quantity'>Now, UniCollab Pro members will earn 50 points</div>
+        <div className='value'>On every ₹100 spent on UniCollab</div>
+    </div>
+</div>
+
             <div className="heading2Wallet">Points History</div>
-            <div className='detailsPayment'>
-                <div className='detailsCardWallet'>
-                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, cumque eum. </div>
-                    <div>Credited On XX-XX-XXXX</div>
+            {pointsHistory.map((entry, index) => (
+                <div className='detailsPayment' key={index}>
+                    <div className='detailsCardWallet'>
+                        <div>{entry.description}</div>
+                        <div>Credited On {entry.date}</div>
+                    </div>
+                    <div className='amountWallet'>+ {entry.points} points</div>
                 </div>
-                <div className='amountWallet'>+ 100 points</div>
-            </div>
-            <div className='detailsPayment'>
-                <div className='detailsCardWallet'>
-                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, cumque eum. </div>
-                    <div>Credited On XX-XX-XXXX</div>
-                </div>
-                <div className='amountWallet'>+ 100 points</div>
-            </div>
-            <div className='detailsPayment'>
-                <div className='detailsCardWallet'>
-                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, cumque eum. </div>
-                    <div>Credited On XX-XX-XXXX</div>
-                </div>
-                <div className='amountWallet'>+ 100 points</div>
-            </div>
+            ))}
         </div>
-    )
+    );
 }
 
-export default Wallet
+export default Wallet;
