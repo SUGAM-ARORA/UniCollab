3e3a96a792a8036681aacd550d4914eb3a2ab54b
Mohammed Junaid <mohammedjunaidj08@gmail.com>
2024-10-13 23:44:03 +0530

    Added conditional logic to render Sign Out button for LoggedInUsers only

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useMemo, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import "./Menu.css";
 import logo from "../img/logo.png";
 import { Link } from "react-router-dom";
